f6a4a76491391f86723d35c16e3f5623dfa6982f
Lagden404 <joao.lagden@gmail.com>
2023-02-04 09:44:34 -0300

    Fixed a lot of things and cleaned up the code

--- Lexer.c
+++ Lexer.c
@@ -4,79 +4,42 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define TOTALTOKENS 31
+#define TOTALTOKENS 32
 
 /*
 types of tokens:
 0 - separators
 1 - operators
-2 - literals
+2 - spaces
 3 - keyword
 4 - special
-
+5 - literals
 */
 struct tokenListElement
 {
     int type;
     char* token;
-} tokenList[TOTALTOKENS];
+};
 
-
-
-const char *getFileAsString(FILE *file);
-bool isAToken (char c, struct tokenListElement* tokenList);
-int getTokenType(char* token, struct tokenListElement* tokenList);
-bool compareTokenWithLists (char* token, char** list);
-
+char *getFileAsString(FILE *file);
+int getTokenType(char* token, char** list);
 
 int main (int argc, char *argv[])
 {
 
     int i;
-    char c;
+    char* fileAsString;
 
     //creates variable for file and opens the file specified in the command line arguments
     FILE *file;
     file = fopen(argv[1], "r");
 
+    //token dictionary array
+    char* tokens[TOTALTOKENS] = {"(", ")", "[", "]", "+", "-", "*", "/", "^", "<", ">", "=", ">=", "<=", "!=", "==", "OR", "AND", "NOT", "input", "print", "if", "elseif", "else", "do", "true", "false", "define", "return", "\n", ";", " "};
 
+    printf("%d\n", getTokenType("", tokens));
 
-    //dictionary of tokens
-    //implement a better way to list tokens and their types
-    tokenList[0].token = "(";
-    tokenList[1].token = ")";
-
-    tokenList[2].token = "+";
-    tokenList[3].token = "-";
-    tokenList[4].token = "*";
-    tokenList[5].token = "/";
-
-    //gets the token type and puts it in the tokenList type
-
-    for (i = 0; i < TOTALTOKENS; i++)
-    {
-        tokenList[i].type = getTokenType(tokenList[i].token, tokenList);
-    }
-
-
-    for (i = 0; (c = getc(file)) != EOF; i++)
-        {
-            if (isAToken(c, tokenList)) {printf("is a token\n");
-        }
-        }
-
-
-
-
-
-    //for (i = 0; i < TOTALTOKENS; i++) printf("%d %s\n", tokenList[i].type, tokenList[i].token);
-
-
-
-    getFileAsString(file);
-
-
-
+    fileAsString = getFileAsString(file);
 
     if (file == NULL)
         {
@@ -103,11 +66,10 @@ int countTokens (FILE *file)
 
     */
 
-
     return numTokens;
 }
 
-const char* getFileAsString(FILE *file)
+char* getFileAsString(FILE *file)
 {
     //copies file to a string
 
@@ -124,58 +86,26 @@ const char* getFileAsString(FILE *file)
 
 };
 
-bool isAToken (char c, struct tokenListElement* tokenList)
+int getTokenType(char* token, char** tokenList)
 {
-    //you have to compare as strings, not characters
-    //or you can make a condition based on its first char
-    int i;
+    int i, indexOnTokenList, comparisonCondition;
 
     for (i = 0; i < TOTALTOKENS; i++)
-        {
-            if (c == tokenList[i].token[0])
-                {
-                    return true;
-                }
-        }
-
-    return false;
-
-}
-
-int getTokenType(char* token, struct tokenListElement* tokenList)
-{
-    int i;
-    //lists for token types
-
-    char *separators[4] = {"(", ")", "[", "]"};
-    char *operators[16] = {"+", "-", "*", "/", "^", "<", ">", "=", ">=", "<=", "!=", "==", "OR", "AND", "NOT"};
-    //literals
-    char *keywords[10] = {"input", "print", "if", "elseif", "else", "do", "true", "false", "define", "return"};
-    char *specials[2] = {"\n", ";"};
-    char *space[1] = {' '};
-
-    //if (compareTokenWithLists(token, separators)) {return 0;} 
-    //else if (compareTokenWithLists(token, operators)) {return 1;} 
-    //else if (compareTokenWithLists(token, keywords)) {return 3;} 
-    //else if (compareTokenWithLists(token, specials)) {return 4;} 
-    //else if (compareTokenWithLists(token, space)) {return 5;}
-    //else return 2;
-
-}
-
-bool compareTokenWithLists (char* token, char** list)
-{
-    int i;
-    size_t listSize = sizeof(list) / sizeof(list[0]);
-    bool isInList = false;
-
-    for (i = 0; i < listSize; i++)
     {
-        if (strcmp(token, list[i]) == 0) isInList = true;
+        comparisonCondition = (strcmp (token, tokenList[i]));
+        
+        if ((comparisonCondition) == 0)
+        {
+            indexOnTokenList = i;
+            break;
+        }
     }
 
-    //segmentation fault
-
-    return isInList;
+    if (i >= 0 && i <= 3) return 0;
+    else if (i >= 4 && i <= 18) return 1;
+    else if (i == 31) return 2;
+    else if (i >= 19 && i <= 28) return 3;
+    else if (i == 29 || i == 30) return 4; 
+    else return 5;
 
 }
